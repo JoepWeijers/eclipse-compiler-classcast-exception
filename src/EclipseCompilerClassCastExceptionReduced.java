2cbcac9aa9f9714e45d07aad49b12925f3b9a38f
Joep Weijers <j.weijers@topdesk.com>
2023-06-01 19:50:04 +0200

    Reduce the example by removing the Jackson dependency

---
+++ src/EclipseCompilerClassCastExceptionReduced.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class EclipseCompilerClassCastExceptionReduced {
+	
+	public static void main(String[] args) throws Exception {
+		List<String> list = null;
+		Object foo = "[\"a\",\"b\"]";
+		// Breaks when "Unnecessary cast or 'instanceof' check" is set to 'Warning'
+//		list = new ObjectMapper2().readValue((String)foo, new TypeReference2<>() { /*  */ });
+		
+		// Explicitly typing the TypeReference works around it
+		list = new ObjectMapper2().readValue((String)foo, new TypeReference2<List<String>>() { /*  */ });
+		System.out.println(list);
+	}
+	
+	private static class TypeReference2<T> implements Comparable<TypeReference2<T>> {
+		@Override
+		public int compareTo(TypeReference2<T> o) {
+			return 0;
+		}
+	}
+
+	private static class ObjectMapper2 {
+		private <T> T readValue(String content, TypeReference2<T> valueTypeRef) {
+			return readValue(content, "");
+		}
+
+		private <T> T readValue(String content, String foo) {
+			return null;
+		}
+	}
+}
